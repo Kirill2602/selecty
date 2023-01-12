54951eeabe6b4fd3d553aacc76b85a7e7d42d4c9
Spitsyn Kirill <kaspicin2602@gmail.com>
2023-01-12 09:22:18 +0300

    reсÑchange steps

--- src/test/java/ru/selecty/SelectyTest.java
+++ src/test/java/ru/selecty/SelectyTest.java
@@ -11,70 +11,70 @@ public class SelectyTest extends TestBase {
 
     @Test
     @DisplayName("Check Main page text")
-    @Step("1")
+    @Step("Проверка текста на главной странице")
     public void checkMainPageText() {
         mainPage.checkMainPageText();
     }
 
     @Test
     @DisplayName("Check NavBar links text")
-    @Step("2")
+    @Step("Проверка наличия необходимых ссылок в нав баре")
     public void checkNavBarLinksText() {
         mainPage.checkNavBarLinksText(getLinkText());
     }
 
     @Test
     @DisplayName("Check NavBar links text after click on EN language link")
-    @Step("3")
+    @Step("Проверка наличия необходимых ссылок в нав баре, после смены языка на английский")
     public void checkNavBarLinksTextAfterClickOnEnLanguageLink() {
         mainPage.clickEnLanguageLink().checkNavBarLinksText(getEnLanguageLinkText());
     }
 
     @Test
     @DisplayName("Check Selecty subMenu links")
-    @Step("4")
+    @Step("Проверка наличия всех необходимых пунктов в выпадающем списке 'Selecty'")
     public void checkSelectySubMenuLinks() {
         mainPage.checkLinkSubMenu("Selecty", getNavBarSubMenuMap().get("Selecty"));
     }
 
     @Test
     @DisplayName("Check Services subMenu links")
-    @Step("5")
+    @Step("Проверка наличия всех необходимых пунктов в выпадающем списке 'Услуги'")
     public void checkServicesSubMenuLinks() {
         mainPage.checkLinkSubMenu("Услуги", getNavBarSubMenuMap().get("Услуги"));
     }
 
     @Test
     @DisplayName("Check Career subMenu links")
-    @Step("6")
+    @Step("Проверка наличия всех необходимых пунктов в выпадающем списке 'Карьера'")
     public void checkCareerSubMenuLinks() {
         mainPage.checkLinkSubMenu("Карьера", getNavBarSubMenuMap().get("Карьера"));
     }
 
     @Test
     @DisplayName("Check Selecty EN language SubMenu links")
-    @Step("7")
+    @Step("Проверка наличия всех необходимых пунктов в выпадающем списке 'Selecty', после смены языка на английский")
     public void checkSelectyEnLanguageSubMenuLinks() {
         mainPage.clickEnLanguageLink().checkLinkSubMenu("Selecty", getEnLanguageNavBarSubMenuMap().get("Selecty"));
     }
 
     @Test
     @DisplayName("Check Services EN language subMenu links")
-    @Step("8")
+    @Step("Проверка наличия всех необходимых пунктов в выпадающем списке 'Services', после смены языка на английский")
     public void checkServicesEnLanguageSubMenuLinks() {
         mainPage.clickEnLanguageLink().checkLinkSubMenu("Services", getEnLanguageNavBarSubMenuMap().get("Services"));
     }
 
     @Test
     @DisplayName("Check Career EN language SubMenu links")
-    @Step("9")
+    @Step("Проверка наличия всех необходимых пунктов в выпадающем списке 'Career', после смены языка на английский")
     public void checkCareerEnLanguageSubMenuLinks() {
         mainPage.clickEnLanguageLink().checkLinkSubMenu("Career", getEnLanguageNavBarSubMenuMap().get("Career"));
     }
 
     @Test
     @DisplayName("Check social links href")
-    @Step("10")
+    @Step("Проверка наличия всех необходимых ссылок на социальные сети компании")
     public void checkSocialLinksHref() {
         mainPage.checkPartOfSocialLinksHref(getExpectedSocialLinks());
     }
